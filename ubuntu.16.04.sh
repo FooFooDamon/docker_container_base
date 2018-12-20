9d9ec79ed352b9348f3e1a6320d66250498ec819
FooFooDamon <udc577@126.com>
2018-12-20 14:30:24 +0800

    所有apt install加上-y选项

--- ubuntu.16.04.sh
+++ ubuntu.16.04.sh
@@ -3,12 +3,12 @@
 [ -z "$USER" ] && SUDO="" || SUDO=sudo
 
 $SUDO apt update
-$SUDO apt install language-pack-zh-hans language-pack-zh-hant
+$SUDO apt install -y language-pack-zh-hans language-pack-zh-hant
 echo "export LANG=zh_CN.UTF-8" >> $HOME/.bashrc
-$SUDO apt install perl expect
-$SUDO apt install gcc g++ make cmake
-$SUDO apt install automake autoconf libtool pkg-config flex bison
-$SUDO apt install vim ctags
-$SUDO apt install git
+$SUDO apt install -y perl expect
+$SUDO apt install -y gcc g++ make cmake
+$SUDO apt install -y automake autoconf libtool pkg-config flex bison
+$SUDO apt install -y vim ctags
+$SUDO apt install -y git
 mkdir -p $HOME/bin $HOME/etc $HOME/include $HOME/lib $HOME/src
 
