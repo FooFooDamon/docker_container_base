ba5cc22247af4783a95b7a0335b96f073ec591e6
FooFooDamon <udc577@udc577>
2019-01-31 14:56:54 +0800

    + apt install xterm

--- ubuntu.16.04.sh
+++ ubuntu.16.04.sh
@@ -7,7 +7,7 @@ $SUDO apt update
 $SUDO apt install -y language-pack-zh-hans language-pack-zh-hant
 [ `grep "LANG=zh_CN\.UTF\-8" $HOME/.bashrc -c` -eq 0 ] && echo "export LANG=zh_CN.UTF-8" >> $HOME/.bashrc
 
-$SUDO apt install -y perl expect
+$SUDO apt install -y perl expect xterm
 
 $SUDO apt install -y gcc g++ make cmake
 $SUDO apt install -y automake autoconf libtool pkg-config flex bison
