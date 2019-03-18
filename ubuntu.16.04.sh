609446226e9fcfaeb29ee2da3321f9a87f423aae
FooFooDamon <udc577@udc577>
2019-03-18 10:00:22 +0800

    屏蔽apport和ctags的安装，屏蔽时区的设置（改为提示挂载宿主机时区文件），增加apt-transport-https和ca-certificates的安装以及DEBIAN_FRONTEND环境变量的设置，将net-tools、curl、wget的安装顺序提前

--- ubuntu.16.04.sh
+++ ubuntu.16.04.sh
@@ -2,9 +2,18 @@
 
 [[ -z "$USER" || "$USER" = "root" ]] && SUDO="" || SUDO=sudo
 
-[ $(ls -l /etc/localtime | grep -i shanghai -c) -gt 0 ] || ln -snf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+echo -e "\e[0;33mNOTE: Add the option of '-v /etc/localtime:/etc/localtime:ro' while running 'docker run' to set the time zone correctly!!!\e[0m" >&2
+#[ $(ls -l /etc/localtime | grep -i shanghai -c) -gt 0 ] || ln -snf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
 $SUDO apt update
+
+# Avoid interactive package installers such as tzdata.
+export DEBIAN_FRONTEND=noninteractive
+
+$SUDO apt install -y apt-transport-https build-essential ca-certificates
+
+# ifconfig in net-tools
+$SUDO apt install -y net-tools curl wget
 
 $SUDO apt install -y language-pack-zh-hans language-pack-zh-hant
 [ `grep "LANG=zh_CN\.UTF\-8" $HOME/.bashrc -c` -eq 0 ] && echo "export LANG=zh_CN.UTF-8" >> $HOME/.bashrc
@@ -12,18 +21,17 @@ $SUDO apt install -y language-pack-zh-hans language-pack-zh-hant
 $SUDO apt install -y perl expect xterm
 
 $SUDO apt install -y gcc g++ make cmake
-$SUDO apt install -y automake autoconf build-essential libtool pkg-config flex bison
-$SUDO apt install -y gdb apport
+$SUDO apt install -y automake autoconf libtool pkg-config flex bison
+$SUDO apt install -y gdb
+#$SUDO apt install -y apport
 [ `grep "ulimit -c unlimited" $HOME/.bashrc -c` -eq 0 ] && echo "ulimit -c unlimited" >> $HOME/.bashrc
 
-$SUDO apt install -y vim ctags
+$SUDO apt install -y vim
+#$SUDO apt install -y ctags
 
 $SUDO apt install -y git
 
 $SUDO apt install -y parallel jq dos2unix zip unzip
-
-# ifconfig in net-tools
-$SUDO apt install -y net-tools curl wget
 
 #$SUDO apt install -y openssh-server
 #$SUDO sed -i "/^[#]\{0,1\}[ ]\{0,\}\(PermitRootLogin \).*/s//\1yes/g" /etc/ssh/sshd_config
